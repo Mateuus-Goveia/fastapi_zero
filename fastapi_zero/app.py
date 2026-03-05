799ee36803a4a4bbf49b389b48b64aa0f349da23
Matheus Goveia <102265029+Mateuus-Goveia@users.noreply.github.com>
2026-03-05 11:18:16 -0300

    Fix: removendo arquivos ignorados do rastreamento

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fastapi_zero.schemas import Message
 
@@ -9,4 +10,17 @@ app = FastAPI()
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá mundo!'}
+    return {'message': 'Olá Mundo!'}
+
+
+@app.get('/ola_mundo', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def ola_mundo():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -8,4 +8,12 @@ def test_root_deve_retornar_ola_mundo():
 
     response = client.get('/')
 
-    assert response.json() == {'message': 'Olá mundo!'}
+    assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_ola_mundo_deve_retornar_um_html():
+    client = TestClient(app)
+
+    response = client.get('/ola_mundo')
+
+    assert '<h1> Olá Mundo </h1>' in response.text
